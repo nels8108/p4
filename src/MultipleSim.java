a65fa3897ea22785cd0ae232758df3230bc5e1aa
Yuancheng Wu <steamedrice11@gmail.com>
2015-04-29 18:45:04 -0500

    clean up

--- src/MultipleSim.java
+++ src/MultipleSim.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -92,30 +91,3 @@ public class MultipleSim {
 	// m.run();
 
 }
-=======
-
-public class MultipleSim {
-	public static void main(String[] args){
-		for(int i = 3; i < 13; i++){
-			ShopperSim n = new ShopperSim();
-			n.setNumOfLanes(i);
-			n.run();
-		}
-////		ShopperSim n = new ShopperSim();
-//		ShopperSim m = new ShopperSim();
-//		
-//		n.setNumOfLanes(7);
-//		m.setNumOfLanes(8);
-//		
-//		
-//		System.out.println("Stats for ShopperSim n");
-//		System.out.println();
-//		n.run();
-//		System.out.println();
-//		System.out.println("Stats for ShopperSim m:");
-//		m.run();
-		
-		
-		}
-	}
->>>>>>> origin/master
